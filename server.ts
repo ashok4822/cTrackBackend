37c8079489264206daacdef663cffc5caca0fb65
arun <arun@gmail.com>
2026-02-11 11:26:40 +0530

    first commit after creating gitignore

---
+++ server.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+//Routes
+// app.use("/api/admin");
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`),
+);
