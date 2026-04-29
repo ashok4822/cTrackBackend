25d6116910ba12f62779972ac98e432ca278df80
Ashok Krishnan <4822ashok@gmail.com>
2026-04-29 14:12:51 +0530

    Delete dist directory

--- dist/src/application/dto/ActivityDto.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// ─── Activity DTOs ────────────────────────────────────────────────────────────
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=ActivityDto.js.map
--- dist/src/application/dto/SupportDto.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// ─── Support Chat DTOs ──────────────────────────────────────────────────────────
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=SupportDto.js.map
